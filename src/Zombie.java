8fcb6c2a3b043add60433b55482df851e5cb3d3c
chro-Mia <tessa.chromia@gmail.com>
2024-11-04 13:43:09 -0800

    initial | complete & ready

---
+++ src/Zombie.java
@@ -0,0 +1,22 @@
+public class Zombies
+{
+    private int x;
+    private int y;
+
+    public Zombie(){
+        x = 100;
+        y = 100;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void walk(){
+        
+    }
+}
